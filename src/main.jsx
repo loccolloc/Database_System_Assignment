4af9072410448834db8eebb4b600179575507e2c
kng1259 <khoinguyenanlac1@gmail.com>
2024-04-06 23:22:52 +0700

    setup react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 // Import css files
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Homepage from "./pages/Homepage.jsx";
+import Login from "./pages/Login.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
